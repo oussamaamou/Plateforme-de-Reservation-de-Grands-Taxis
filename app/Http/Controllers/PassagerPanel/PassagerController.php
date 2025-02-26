15c9b80f9f7e254fb11366d898f2f09227fa09ff
oussamaamou <amoussdark@gmail.com>
2025-02-26 01:08:22 +0000

    Gestion des roles et affichage des dashboards

--- app/Http/Controllers/PassagerPanel/PassagerController.php
+++ app/Http/Controllers/PassagerPanel/PassagerController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\PassagerPanel;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PassagerController extends Controller
 {
     public function index(){
+
+        if(Auth::user()->role === 'Administrateur' ){
+
+            return redirect()->route('admin.dashboard');
+        }
+
+        elseif(Auth::user()->role === 'Chauffeur' ){
+
+            return redirect()->route('chauffeur.dashboard');
+        }
         
-        return view('passager.dashboard');
+        return view('dashboard');
     }
 }
