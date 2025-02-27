0dbeaa4c778e77e62920d8c393bcc623819c435e
oussamaamou <amoussdark@gmail.com>
2025-02-27 09:44:24 +0000

    Modification et Ajout des controlleurs

--- app/Http/Controllers/ChauffeurPanel/ChauffeurController.php
+++ app/Http/Controllers/ChauffeurPanel/ChauffeurController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\ChauffeurPanel;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ChauffeurController extends Controller
@@ -10,5 +11,16 @@ class ChauffeurController extends Controller
     public function index(){
         
         return view('chauffeur.dashboard');
+    }
+
+    public function reservations(){
+
+        return view('chauffeur.reservations');
+    }
+
+    public function getAllChauffeur(){
+
+        $chauffeurs = User::where('role', 'Chauffeur')->get();
+        return view('dashboard', ['chauffeurs' => $chauffeurs]);
     }
 }
