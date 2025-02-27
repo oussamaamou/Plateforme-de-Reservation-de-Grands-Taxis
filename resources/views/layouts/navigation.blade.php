3072dc09e9dfff8e9cb19fe14c430cfb634e908e
oussamaamou <amoussdark@gmail.com>
2025-02-27 09:46:35 +0000

    Modification et Ajout des layouts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link href="{{ route('passager.mesreservations') }}" :active="request()->routeIs('passager.mesreservations')">
+                        Mes Reservations
+                    </x-nav-link>
+
                 </div>
             </div>
 
