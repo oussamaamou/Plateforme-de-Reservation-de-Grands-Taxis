a0875f6cb3a8f6a5e22a026dc699a6c9bc7fe26c
oussamaamou <amoussdark@gmail.com>
2025-02-27 09:47:07 +0000

    Modification et Ajout des views

--- resources/views/passager/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<h1> Passager Dashboard</h1>
-
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-
-    <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
-        Logout
-    </a>
-</form>
