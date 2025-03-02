11160fe32bd9f1acb387b4eb831c07e2034847f0
oussamaamou <amoussdark@gmail.com>
2025-03-02 15:58:28 +0000

    Ajout et Modifiation des tableaux

--- app/Models/Trajet.php
+++ app/Models/Trajet.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Trajet extends Model
 {
     use HasFactory;
+
+    // public $timestamps = false;
+
+    protected $fillable = [
+        'dateDepart',
+        'dateArrivee',
+        'lieuDepart',
+        'lieuArrivee',
+        'passager_id',
+        'chauffeur_id'
+    ];
+
+    public function passager(){
+        
+        return $this->belongsTo(User::class, 'passager_id');
+    }
+
+    public function chauffeur(){
+
+        return $this->belongsTo(User::class, 'chauffeur_id');
+    }
 }
