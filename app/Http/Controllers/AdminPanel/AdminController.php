43edd0dd4c76fecae3a9f73907799990a044ba59
oussamaamou <amoussdark@gmail.com>
2025-03-02 15:50:02 +0000

    Modification et Ajout des controleurs

--- app/Http/Controllers/AdminPanel/AdminController.php
+++ app/Http/Controllers/AdminPanel/AdminController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers\AdminPanel;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Trajet;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index(){
+
+        $totalTrajets = $this->getTotalTrajets();
+        $totalUsers = $this->getTotalUsers();
+        $totalChauffeurs = $this->getTotalChauffeurs();
+        $totalPassagers = $this->getTotalPassagers();
+        $trajets = Trajet::all();
         
-        return view('admin.dashboard');
+        return view('admin.dashboard', compact('totalTrajets', 'totalUsers', 'totalChauffeurs', 'totalPassagers', 'trajets'));
     }
+
+    private function getTotalTrajets(){
+        return Trajet::count();
+    }
+
+    private function getTotalUsers(){
+        return User::count();
+    }
+
+    private function getTotalChauffeurs(){
+        return User::where('role', 'Chauffeur')->count();
+    }
+
+    private function getTotalPassagers(){
+        return User::where('role', 'Passager')->count();
+    }
+
+    public function getAllUsers(){
+
+        $chauffeurs = User::where('role', 'Chauffeur')->get();
+        $passagers = User::where('role', 'Passager')->get();
+
+        return view('admin.userslistes', compact('chauffeurs', 'passagers'));
+    }
+
+    
 }
