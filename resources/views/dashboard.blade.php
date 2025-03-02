1ab7fe0d59040c88e5f909d6401d00682d20a317
oussamaamou <amoussdark@gmail.com>
2025-03-02 15:52:48 +0000

    Ajout et Modifiation des vues

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,11 +25,11 @@
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-stone-700 md:text-2xl dark:text-white">
                             Reserver votre Chauffeur
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="reservations" method="POST">
+                        <form class="space-y-4 md:space-y-6" action="{{route('create.reservation')}}" method="POST">
                             @csrf
 
                             <input type="hidden" name="passager_id" value="{{ Auth::user()->id }}">
-                            <input type="hidden" name="chauffeur_id" value="">
+                            <input type="hidden" id="chauffeur_id" name="chauffeur_id" value="">
 
                             <input type="hidden" name="medecin_id" id="medecin_id" value="">
                             <div class="mb-5">
@@ -45,16 +45,16 @@
                             <div>
                                 <label for="lieuDepart" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lieu de Depart</label>
                                 <select id="lieuDepart" name="lieuDepart" required class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option value="Patient">Safi</option>
-                                    <option value="Medecin">Casablanca</option>
+                                    <option value="Safi">Safi</option>
+                                    <option value="Casablanca">Casablanca</option>
                                 </select>
                             </div>
 
                             <div>
                                 <label for="lieuArrivee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lieu d'Arrivée</label>
                                 <select id="lieuArrivee" name="lieuArrivee" required class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option value="Patient">Safi</option>
-                                    <option value="Medecin">Casablanca</option>
+                                    <option value="Safi">Safi</option>
+                                    <option value="Casablanca">Casablanca</option>
                                 </select>
                             </div>
 
@@ -70,10 +70,10 @@
             <div class="mt-[4rem] ml-[12rem] bg-white dark:bg-gray-800 rounded-xl shadow-2xl max-w-4xl w-full p-8 transition-all duration-300 animate-fade-in pt-[3.5rem]">
                 <div class="flex flex-col md:flex-row">
                     <div class="md:w-1/3 text-center mb-8 md:mb-0">
-                        <img src="https://static.vecteezy.com/system/resources/previews/033/397/041/non_2x/taxi-driver-profession-during-work-concept-young-smiling-handsome-man-taxi-driver-sitting-in-yellow-car-and-looking-from-window-during-job-vector.jpg" alt="Profile Picture" class="rounded-full w-48 h-48 mx-auto mb-4 border-4 border-amber-500 transition-transform duration-300 hover:scale-105">
+                        <img src="{{ $chauffeur->photo }}" alt="Profile Picture" class="rounded-full w-48 h-48 mx-auto mb-4 border-4 border-amber-500 transition-transform duration-300 hover:scale-105">
                         <h1 class="text-xl font-bold text-amber-500 dark:text-white mb-2">{{ $chauffeur->name }} {{ $chauffeur->last_name }}</h1>
                         <p class="text-stone-700 font-bold">{{ $chauffeur->role }}</p>
-                        <button onclick="addConsultationForm()" class="mt-4 font-medium text-white px-4 py-2 rounded-lg bg-gradient-to-r from-amber-400 via-amber-500 to-amber-600 hover:bg-gradient-to-br transition-colors duration-300">Reserver</button>
+                        <button onclick="addReservationForm({{ $chauffeur->id }})" class="mt-4 font-medium text-white px-4 py-2 rounded-lg bg-gradient-to-r from-amber-400 via-amber-500 to-amber-600 hover:bg-gradient-to-br transition-colors duration-300">Reserver</button>
                     </div>
                     <div class="md:w-2/3 md:pl-8">
                         
@@ -90,7 +90,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-stone-700 " viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                                 </svg>
-                                
+                                {{ $chauffeur->telephone }}
                             </li>
                         </ul>
                     </div>
@@ -107,7 +107,7 @@
 
 <script>
 
-function addConsultationForm(chauffeur_id) {
+function addReservationForm(chauffeur_id) {
 
     document.getElementById("chauffeur_id").value = chauffeur_id;
     document.getElementById("ctnrcsltion").classList.remove('hidden');
